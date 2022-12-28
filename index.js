9ae78f502b57edecdcaabe61dc72680ca163dea8
Digambaran <arjunsreekumar.1@gmail.com>
2022-12-28 17:57:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const username_verify = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello username_verify`}))
+  res.end()
+  
+}
+
+export default username_verify
